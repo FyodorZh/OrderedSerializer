890e59a2f59ead8b41bd1ef44466cd72d8f655b5
FyodorZh <fese@list.ru>
2023-11-30 22:49:09 +0300

    Guid based generic type serialization

--- Package/Runtime/OrderedSerializer/TypeSerialization/Implementations/GuidBased/GuidBasedTypeDeserializer.cs
+++ Package/Runtime/OrderedSerializer/TypeSerialization/Implementations/GuidBased/GuidBasedTypeDeserializer.cs
@@ -11,6 +11,9 @@ namespace OrderedSerializer.TypeSerializers
 
         private readonly Dictionary<string, Type> _types = new Dictionary<string, Type>();
 
+        [ThreadStatic]
+        private static List<string>? _ids;
+
         public GuidBasedTypeDeserializer()
         {
         }
@@ -57,14 +60,93 @@ namespace OrderedSerializer.TypeSerializers
         public Type? Deserialize(IReader reader)
         {
             byte version = reader.ReadByte();
-            string? guid = reader.ReadString();
-
-            if (guid != null && _types.TryGetValue(guid, out var type))
+            switch (version)
             {
-                return type;
+                case 0:
+                {
+                    _ids ??= new List<string>();
+                    _ids.Clear();
+                    byte count = reader.ReadByte();
+                    for (int i = 0; i < count; ++i)
+                    {
+                        _ids.Add(reader.ReadString() ?? "");
+                    }
+
+                    int pos = 0;
+                    return ConstructTypeRecursive(_ids, ref pos);
+                }
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+
+        private Type? ConstructTypeRecursive(List<string> ids, ref int pos)
+        {
+            string? typeId = ids[pos++];
+            if (typeId == null)
+            {
+                return null;
             }
 
-            return null;
+            Type? type = GetTypeById(typeId);
+            if (type == null)
+            {
+                return null;
+            }
+
+            if (type.IsGenericType)
+            {
+                var genericArguments = new Type[type.GetGenericArguments().Length];
+                for (int i = 0; i < genericArguments.Length; ++i)
+                {
+                    var typeArg = ConstructTypeRecursive(ids, ref pos);
+                    if (typeArg == null)
+                    {
+                        return null;
+                    }
+
+                    genericArguments[i] = typeArg;
+                }
+                type = type.MakeGenericType(genericArguments);
+            }
+
+            return type;
+        }
+
+        private Type? GetTypeById(string id)
+        {
+            switch (id)
+            {
+                case "Boolean":
+                    return typeof(Boolean);
+                case "Byte":
+                    return typeof(Byte);
+                case "SByte":
+                    return typeof(SByte);
+                case "Int16":
+                    return typeof(Int16);
+                case "UInt16":
+                    return typeof(UInt16);
+                case "Int32":
+                    return typeof(Int32);
+                case "UInt32":
+                    return typeof(UInt32);
+                case "Int64":
+                    return typeof(Int64);
+                case "UInt64":
+                    return typeof(UInt64);
+                case "Char":
+                    return typeof(Char);
+                case "Double":
+                    return typeof(Double);
+                case "Single":
+                    return typeof(Single);
+                case "String":
+                    return typeof(String);
+                default:
+                    _types.TryGetValue(id, out var type);
+                    return type;
+            }
         }
     }
 }
--- Package/Runtime/OrderedSerializer/TypeSerialization/Implementations/GuidBased/GuidBasedTypeSerializer.cs
+++ Package/Runtime/OrderedSerializer/TypeSerialization/Implementations/GuidBased/GuidBasedTypeSerializer.cs
@@ -1,20 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
 namespace OrderedSerializer.TypeSerializers
 {
     public class GuidBasedTypeSerializer : ITypeSerializer
     {
+        [ThreadStatic] private static List<string>? _ids;
+        
         public void Serialize(IWriter writer, Type type)
         {
+            _ids ??= new List<string>();
+            _ids.Clear();
+            EncodeTypeRecursive(_ids, type);
+            
+            writer.WriteByte(0); // version for the future
+            writer.WriteByte(checked((byte)_ids.Count));
+            foreach (var id in _ids)
+            {
+                writer.WriteString(id);
+            }
+        }
+
+        private static void EncodeTypeRecursive(List<string> ids, Type type)
+        {
+            string typeId = GetTypeId(type);
+            ids.Add(typeId);
+            if (type.IsGenericType)
+            {
+                foreach (var argument in type.GenericTypeArguments)
+                {
+                    EncodeTypeRecursive(ids, argument);
+                }
+            }
+        }
+
+        private static string GetTypeId(Type type)
+        {
+            if (type.IsPrimitive)
+            {
+                return type.Name;
+            }
+
+            if (type.Name == "String")
+            {
+                return "String";
+            }
+            
             var attribute = (GuidAttribute)Attribute.GetCustomAttribute(type, typeof(GuidAttribute), false);
             if (attribute == null)
             {
                 throw new InvalidOperationException($"'{type}' must have GUID attribute");
             }
 
-            writer.WriteByte(0); // version for the future
-            writer.WriteString(attribute.Value);
+            return attribute.Value;
         }
     }
 }
